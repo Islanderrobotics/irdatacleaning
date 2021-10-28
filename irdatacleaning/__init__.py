d63a9a0a78ceeabdf98e8fee651724d52c235596
Islanderrobotics <75331800+Islanderrobotics@users.noreply.github.com>
2021-10-27 21:02:28 -0400

    added string to date

--- irdatacleaning/__init__.py
+++ irdatacleaning/__init__.py
@@ -6,4 +6,5 @@ from .encoder2 import Encoder
 from .missingvalues import MissingValues
 from .featur_scaling import FeatureScaler
 from .inconsistent_data import InconsistentData
+from .stringtodatetime import StringToDateTime
 from .resources import Resources
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.0'
+VERSION = '2021.0.0'
 DESCRIPTION = 'module designed to make your data preprocessing experience easier'
 with open("README.md") as description:
     LONG_DESCRIPTION = description.read()
@@ -20,14 +20,14 @@ setup(
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.8",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
         "Topic"::"Machine Learning",
-        "Topic"::"Data Cleaning"
-        "Topic":: "Data Science"
+        "Topic"::"Data Cleaning",
+        "Topic":: "Data Science",
     ]
 )
