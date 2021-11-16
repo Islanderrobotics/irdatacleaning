140df2eb2187f2f7328d79d3b1add1e967d02e5f
Islanderrobotics <75331800+Islanderrobotics@users.noreply.github.com>
2021-11-15 21:00:28 -0500

    updating the youtube links

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '2021.0.3'
+VERSION = '2021.0.6'
 DESCRIPTION = 'module designed to make your data preprocessing experience easier'
 with open("README.md") as description:
     LONG_DESCRIPTION = description.read()
